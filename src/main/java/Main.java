d69c2a495bb50f4e13fd4f864d3224ecd4865cc7
bartekwazny2005-cmd <bartekwazny2005@gmail.com>
2026-01-19 08:09:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    try {
+      boolean kontynuuj = true;
+      while (kontynuuj) {
+        Service s = new Service();
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        switch (number) {
+          case 1:
+            System.out.println("Podaj imię nowego studenta:");
+            String name = scanner.nextLine(); // 4.1
+            System.out.println("Podaj wiek:");
+            int age = scanner.nextInt();
+            scanner.nextLine();
+            s.addStudent(new Student(name, age));
+            break;
+
+          case 2:
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.ToString()); // 4.2
+            }
+            break;
+
+          case 3:
+            System.out.println("Zakończono program. Do zobaczenia!");
+            kontynuuj = false; //wyjscie z petli 
+            break;
+
+          default:
+            System.out.println("Nieprawidłowa opcja!");
+        }
+
+        if (kontynuuj) {
+          System.out.println();
+          System.out.print("Czy chcesz wykonać kolejne działanie? (t/n): ");
+          char odp = scanner.next().toLowerCase().charAt(0);
+          if (odp != 't') {
+            kontynuuj = false;
+            System.out.println("Zakończono program. Do zobaczenia!");
+          }
+          System.out.println();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      scanner.close();
+    }
+  }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+
+  private String Name;
+  private int Age;
+  private String Date;
+
+  public Student(String name, int age, String Date) {
+    Name = name;
+    Age = age;
+    this.Date = date;
+  }
+
+  public String GetName() {return Name;}
+  public int GetAge() {return Age;}
+  public String GetData() {return Date;}
+
+  public String ToString() {
+    return Name + " " + Integer.toString(Age);
+  }
+
+  public static Student Parse(String str) {
+    String[] data = str.split(" ");
+    if(data.length != 2) 
+      return new Student("Parse Error", -1);
+    return new Student(data[0], Integer.parseInt(data[1]));
+  }
+}
